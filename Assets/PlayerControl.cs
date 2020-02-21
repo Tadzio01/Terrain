77910ff9589735bde1d45cdb09664ec5f8648872
Tadzio <tadziocancilla@gmail.com>
2020-02-21 12:27:56 -0800

    Basic generation and controls

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    private float speed = 5;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(move*speed,0,0);
     }
 }
--- Assets/TerrainGeneration.cs
+++ Assets/TerrainGeneration.cs
@@ -5,42 +5,40 @@ using SimplexNoise;
 
 public class TerrainGeneration : MonoBehaviour
 {
+    private int yScale = 2;
+    private int xScale = 10;
+    private int step = 0;
     float noise(int x, float y) {
-        return SimplexNoise.Noise.CalcPixel1D(x,1/y);
-    }
-    string stringrep(string s, int y) {
-        string rs = "";
-        for(int i = 1; i <= y; i++) {
-            rs = rs + s;
-        }
-        return rs;
+        return SimplexNoise.Noise.CalcPixel2D(x/xScale,0,1/(y/yScale))/255;
     }
     // Start is called before the first frame update
     void Start() {
         SimplexNoise.Noise.Seed = 0;
-        for(int i = 1; i <= 50; i++) {
-            float pm = 3+3*noise(i,900);
-            float om = 35+35*noise(i,1000);
-            float l2 = pm+pm*noise(i,20);
-            float mh = 70+70*noise(i,40);
-            float mmult = (1+noise(i,500))/2;
-            float md = Mathf.Pow((7*noise(i,5))*mmult,10f);
-            float mm = (mh*(Mathf.Pow(mmult,17)))+md;
-            float fd = pm+pm*noise(i,10);
-            int height = Mathf.FloorToInt(fd+om+l2+mm);
-            float tmult = (100+100*noise(i,300))/8;
-            string s = new String("#",height);
-            Debug.Log(s);
-
-            //Instantiate(GameObject.Find("TerrainObject"),new Vector3(i,height+1,0),Quaternion.identity);
-            //for(int i2 = height; i2 >= 0; i2-=1) {
-             // Instantiate(GameObject.Find("TerrainObject"),new Vector3(i,i2,0),Quaternion.identity);  
-           // }
+        for(int i = 1; i <= 500; i++) {
+            
         }
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if(step < 1000){
+            for (int i = 1; i < 3; i++) {
+                step++;
+                float pm = 3+3*noise(step,900);
+                float om = 35+35*noise(step,1000);
+                float l2 = pm+pm*noise(step,20);
+                float mh = 70+70*noise(step,40);
+                float mmult = (1+noise(step,500))/2;
+                float md = (7*noise(step,5))*Mathf.Pow(mmult,10f);
+                float mm = (mh*(Mathf.Pow(mmult,17)))+md;
+                float fd = pm+pm*noise(step,10);
+                int height = Mathf.FloorToInt(fd+om+l2+mm);
+                float tmult = (100+100*noise(step,300))/8;
+                Instantiate(GameObject.Find("TerrainObject"),new Vector3(step,height+1,0),Quaternion.identity);
+                for(int i2 = height; i2 >= 0; i2-=1) {
+                    Instantiate(GameObject.Find("TerrainObject"),new Vector3(step,i2,0),Quaternion.identity);  
+                }
+            }
+        }
     }
 }
