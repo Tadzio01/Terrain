32155a81fda088b96fb413e2a2a9ca220ce6ea31
Tadzio <tadziocancilla@gmail.com>
2020-02-25 10:44:01 -0800

    Player visual and controll adjustment

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,7 +9,11 @@ public class PlayerControl : MonoBehaviour
     private float jumpcool;
 
     bool Isgrounded() {
-        return Physics.Raycast(transform.position, -Vector3.up, 1.1f + 0.1f);
+        float sy = transform.localScale.y/2;
+        bool r1 = Physics.Raycast(transform.position, -Vector3.up, sy);
+        bool r2 = Physics.Raycast(transform.position+new Vector3(0.5f,0,0),-Vector3.up,sy);
+        bool r3 = Physics.Raycast(transform.position-new Vector3(0.5f,0,0),-Vector3.up,sy);
+        return (r1 || r2 || r3);
     }
     void Start()
     {
@@ -21,7 +25,7 @@ public class PlayerControl : MonoBehaviour
     {
         Vector3 dirVector = new Vector3(Input.GetAxis("Horizontal")*speed, 0, 0);
         rb.MovePosition(transform.position+dirVector*Time.fixedDeltaTime);
-        if (Input.GetKeyDown("space") && Isgrounded() && Time.time-jumpcool > 0.1)
+        if ((Input.GetKeyDown("space") || Input.GetKeyDown("w")) && Isgrounded() && Time.time-jumpcool > 0.1)
         {
             rb.AddForce(transform.up * 15,ForceMode.VelocityChange);
             jumpcool = Time.time;
